c8e1ddeb5370fd2910cb73675e53a7909e7d1974
Morados <moradosmontcho946@gmail.com>
2026-06-11 20:51:15 +0100

    Force redeploy Vercel

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://loup-garou-online-frontend.vercel.app"],,
+    origin: ["http://localhost:3000", "https://loup-garou-online.vercel.app"],
     methods: ["GET", "POST"]
   }
 });
